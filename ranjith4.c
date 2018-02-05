4ffa0e194e66594fe5c3c79a4f32096b39ec8a9c
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-05 16:43:42 +0530

    Create ranjith4.c

---
+++ ranjith4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char i.j;
+clrscr();
+printf("enter the alphabet");
+scanf("%d",&i);
+printf("enter the no values");
+scanf("%d",&j);
+getch();
+return 0;
+}
