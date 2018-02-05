8aceb85c149ee127dda864ff341bb1a51669bc69
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-05 16:14:08 +0530

    Create ranjith1.c

---
+++ ranjith1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the odd number")
+scanf("%c",&a);
+printf("enter the even number")
+scanf("%c",&b);
+printf("enter if odd or even")
+getch();
+return 0;
+}
