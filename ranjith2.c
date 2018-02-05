5e0465e37cefd022f241556498fea6e4c3a396c0
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-05 16:25:36 +0530

    Create ranjith2.c

---
+++ ranjith2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char i,j;
+clrscr();
+printf("enter the vowels letter")
+scanf("%d",&i);
+printf("enter the consonent letter")
+scanf("%d",&j);
+getch();
+return 0;
+}
