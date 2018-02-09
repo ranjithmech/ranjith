054833aa1a77d809c6215d5b473cc829a8cf846d
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-09 13:55:46 +0530

    Create ranjith7.c

---
+++ ranjith7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the alphabet")
+scanf("%d",&a);
+printf("enter the no's")
+scanf("%d",&b);
+getch();
+return 0;
+}
