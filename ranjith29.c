7be8cf24d600d5d309c34289cfda60e54ded567c
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-09 13:42:31 +0530

    Create ranjith29.c

---
+++ ranjith29.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the alphabet letter")
+scanf("%d",&a);
+printf("enter the consonant")
+scanf("%d",&b);
+getch();
+return 0;
+}
