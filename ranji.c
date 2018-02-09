46fd06a78e6714076c8dadf00886b6744245be2d
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-09 14:12:52 +0530

    Create ranji.c

---
+++ ranji.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter the values");
+scanf("%d",&a,&b,&c);
+printf("enter the largest values");
+scanf("%d",&d);
+getch();
+return 0;
+}
