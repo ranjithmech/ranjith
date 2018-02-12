6c6f66b94426d9d65bb0117adf4466df00cfb223
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-12 16:09:30 +0530

    Create ranjim12.c

---
+++ ranjim12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the sum of values");
+scanf("%d",&a);
+printf("enter the sum of values");
+scanf("%d",&b);
+c=a+b
+printf("enter the total values");
+scanf("%d",&c);
+getch();
+return 0;
+}
