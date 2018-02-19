0f3b9e22ae0219a9881fe15a43f13190798cc62e
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-19 16:01:11 +0530

    Create mech19.c

---
+++ mech19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the square  values");
+scanf("%d",&a);
+printf("enter the total values");
+scanf("%d",&b);
+b=a*a
+getch();
+return 0;
+}
