372d7c40eff7e794772951e2818919108190add1
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-19 16:24:47 +0530

    Create mechan19.c

---
+++ mechan19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the palindrome number yes");
+scanf("%d",&a);
+printf("enter the palindrome number no");
+scanf("%d",&b);
+getch();
+return 0;
+}
