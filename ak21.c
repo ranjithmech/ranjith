60e7df1e964feb5b77adef7e6c71fc3cdb971818
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-21 16:07:38 +0530

    Create ak21.c

---
+++ ak21.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the two number");
+scanf("%d",&a);
+printf("enter the interval between two number");
+scanf("%d",&b);
+getch();
+return 0;
+}
