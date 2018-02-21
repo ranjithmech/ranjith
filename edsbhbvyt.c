eeca0e17cbe56cd7254eee7f857130360ed74fa1
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-21 16:20:53 +0530

    Create edsbhbvyt.c

---
+++ edsbhbvyt.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the two numbers");
+scanf("%d",&a);
+printf("enter the interval values b\w two");
+scanf("%d",&b);
+getch();
+return 0;
+]
