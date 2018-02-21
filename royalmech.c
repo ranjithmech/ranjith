fda1f44b1cd46784d46a596b360ad5e7b64cf1d7
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-02-21 15:50:49 +0530

    Create royalmech.c

---
+++ royalmech.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the prime number");
+scanf("%d",&a);
+printf("whether the number is prime or not")';
+scanf("%d",&b);
+getch();
+return 0;
+}
