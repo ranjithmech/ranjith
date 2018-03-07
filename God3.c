412a2cda3c32c5e3a14ef02115a021bbb3918425
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-03-07 16:06:59 +0530

    Create God3.c

---
+++ God3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,j,n,temp;
+printf("enter limit");
+scanf("%d",&n);
+printf(" enter the number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("sorted array");
+for(i=0;i<n;i++)
+{
+for(j=1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}}
+}
+for(i=0;i<n;i++)
+{
+printf("%d\n",a[i]);
+}
+}
+getch();
+return 0;
+}
