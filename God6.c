f34587892c877c0aba681c6b5a4a78fb02203821
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-03-12 16:19:35 +0530

    Create God6.c

---
+++ God6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the character");
+scanf("%d",&a);
+printf("count the character");
+scanf("%d",&b);
+getch();
+return 0;
+}
