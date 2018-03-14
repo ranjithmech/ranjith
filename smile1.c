85c81637e5448058262d5f04bed30ee098e19352
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-03-14 03:24:13 -0700

    Create smile1.c

---
+++ smile1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the word");
+scanf("%d",&a);
+printf("enter the space between the word");
+scanf("%d",&b);
+getch();
+return 0;
+}
