2960c364803a436408fd84151c4284c6c70349bf
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-03-16 03:21:40 -0700

    Create smile4.c

---
+++ smile4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter the special characters");
+scanf("%d",&a);
+printf("enter the count");
+scanf("%d",&b);
+getch();
+return 0;
+}
