61e292d8a627b50bbe0316f0b2298a9710bfd916
ranjithmech <36154874+ranjithmech@users.noreply.github.com>
2018-03-23 15:53:28 +0530

    Create smile10.c

---
+++ smile10.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include
+void main()
+{
+char str[30],str1[40];
+clrscr();
+int i,j,flag=0;
+printf("enter 2 strings");
+gets(str);
+gets(str1);
+for(i=0;str[i]!='\0';i++)
+{
+for(j=0;str1[j]!='\0';j++)
+{
+if(str[i]==str1[j])
+{
+flag=0;
+}
+else
+{
+flag=1;
+}
+}
+}
+if(flag==0)
+{
+puts(str);
+}
+else
+{
+puts(str1);
+}
+getch();
+}
